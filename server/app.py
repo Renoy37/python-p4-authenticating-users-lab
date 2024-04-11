71f7486b2a86385f95db1f225002a2489ef79b48
Renoy37 <robinmgk077@gmail.com>
2024-04-11 06:46:47 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -48,9 +48,46 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+    
+    def post(self):
+        user = User.query.filter (
+            User.username == request.get_json()['username']
+        ).first()
+        
+        if user is None:
+            return {'message': 'User not found'}, 404
+        
+        session['user_id'] = user.id
+        return user.to_dict(), 200
+        
+class Logout(Resource):
+    
+    def delete(self):
+        session['user_id'] = None
+        return {'message' : '204: No Content'}, 204
+    
+class CheckSession(Resource):
+    
+    def get(self):
+        user_id = session.get('user_id')
+        
+        if user_id is not None:
+            user = User.query.get(user_id)
+            if user:
+                return user.to_dict(), 200
+            else:
+                return {'message': 'User not found'}, 404
+        else:
+            return {}, 401
+
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 
 if __name__ == '__main__':
